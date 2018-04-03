14791e5afb4573e744d0f63dd83878db1832af9f
jingkong <87647703@qq.com>
2018-04-03 13:21:25 +0800

    login.html
    更换了 login页面

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/loginx.html'), name='login'),
 
 ]
