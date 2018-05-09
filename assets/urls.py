dfaa7452dfcf3fbf85906f306634a89dded41502
jingkong <87647703@qq.com>
2018-05-09 08:44:23 +0800

    11
    11

--- assets/urls.py
+++ assets/urls.py
@@ -16,10 +16,8 @@ urlpatterns = [
     path('port/map/list/', views.PortMapListView.as_view(), name='port_map_list'),
     # 添加端口映射
     path('port/map/add/', views.AddPortMapView.as_view(), name='add_port_map'),
-
     # 删除映射记录
     path('port/delete/<int:p_id>/', views.DeletePortMapView.as_view(), name='delete_port'),
-
     # 修改映射记录
     path('port/map/change/<int:p_id>/', views.ChangePortMapView.as_view(), name='change_port'),
 ]
